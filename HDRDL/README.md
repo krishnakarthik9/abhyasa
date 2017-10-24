03d1159d237045cfb464b34163c714431e744c2a
Harsha Nalluru <harsha.nalluru1997@gmail.com>
2017-10-24 10:32:51 +0530

    HDRDL FOLDER ===> added code for svm, cnn, knn, random_forest classifiers and tested for two classes

---
+++ HDRDL/README.md
@@ -0,0 +1,31 @@
+# Handwritten Digit Recognition using Machine Learning and Deep Learning
+
+## Published Paper 
+
+[IJARCET-VOL-6-ISSUE-7-990-997](http://ijarcet.org/wp-content/uploads/IJARCET-VOL-6-ISSUE-7-990-997.pdf)
+
+## Accuracy using Machine Learning Algorithms:
+
+i)	 K Nearest Neighbors: 96.67%
+
+ii)	 SVM:	97.91%
+
+iii) Random Forest Classifier:	96.82%
+
+
+## Accuracy using Deep Neural Networks:
+
+i)	Three Layer Convolutional Neural Network using Tensorflow:	99.70%
+
+ii)	Three Layer Convolutional Neural Network using Keras and Theano: 98.75%
+
+**All code written in Python 3.5. Code executed on Intel Xeon Processor / AWS EC2 Server.**
+
+## Video Link:
+```
+https://www.youtube.com/watch?v=7kpYpmw5FfE
+```
+
+## Test Images Classification Output:
+
+![Output a1](Outputs/output.png?raw=true "Output a1")       
---
+++ HDRDL/SVM/svm.py
@@ -0,0 +1,188 @@
+import sys
+import numpy as np
+import pickle
+from sklearn import model_selection, svm, preprocessing
+from sklearn.metrics import accuracy_score,confusion_matrix
+from MNIST_Dataset_Loader.mnist_loader import MNIST
+import matplotlib.pyplot as plt
+from matplotlib import style
+import os
+from PIL import Image 
+import numpy as np
+import PIL
+np.set_printoptions(threshold=np.nan)
+style.use('ggplot')
+
+
+# Save all the Print Statements in a Log file.
+old_stdout = sys.stdout
+log_file = open("summary.log","w")
+#sys.stdout = log_file
+
+# Load MNIST Data
+print('\nLoading MNIST Data...')
+data = MNIST('./MNIST_Dataset_Loader/')
+
+features = []	
+labels = []
+count = 1000
+for file in os.listdir("A"):
+	try:
+		img = np.asarray(Image.open("A/" + file).convert('L').resize((45,45), Image.ANTIALIAS)).flatten()
+		features.append(img)
+		labels.append('A')
+		count = count -1
+		if count == 0:
+			break
+	except Exception as e:
+		print(e)
+		pass
+print(features[0])
+count = 1000
+for file in os.listdir("equal"):
+	try:
+		img = np.asarray(Image.open("equal/" + file).convert('L').resize((45,45), Image.ANTIALIAS)).flatten()
+		features.append(img)
+		labels.append('equal')
+		count = count -1
+		if count == 0:
+			break
+	except Exception as e:
+		print(e)
+		pass
+
+print('\nLoading Training Data...')
+#img_train, labels_train = data.load_training()
+#print('type of img_train = ',type(img_train[0]),img_train[0])
+
+#train_img = np.array(img_train)
+train_img = np.array(features)
+train_labels = np.array(labels)
+#train_labels = np.array(labels_train)
+
+print('\nLoading Testing Data...')
+#img_test, labels_test = data.load_testing()
+
+
+
+test_features = []	
+labels_test = []
+count = 0
+for file in os.listdir("A"):
+	try:
+		if count < 1001:
+			count += 1
+			continue
+		if count > 1200:
+			break
+		img = np.asarray(Image.open("A/" + file).convert('L').resize((45,45), Image.ANTIALIAS)).flatten()
+		test_features.append(img)
+		labels_test.append('A')
+		count += 1
+	except Exception as e:
+		print(e)
+		pass
+count = 0
+for file in os.listdir("equal"):
+	try:
+		if count < 1001:
+			count += 1
+			continue
+		if count > 1200:
+			break
+		img = np.asarray(Image.open("equal/" + file).convert('L').resize((45,45), Image.ANTIALIAS)).flatten()
+		test_features.append(img)
+		labels_test.append('equaequal')
+		count += 1
+	except Exception as e:
+		print(e)
+		pass
+print(test_features[0])
+
+test_img = np.array(test_features)
+test_labels = np.array(labels_test)
+
+
+#Features
+X = train_img
+
+#Labels
+y = train_labels
+
+# Prepare Classifier Training and Testing Data
+print('\nPreparing Classifier Training and Validation Data...')
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.1)
+
+
+# Pickle the Classifier for Future Use
+print('\nSVM Classifier with gamma = 0.1; Kernel = polynomial')
+print('\nPickling the Classifier for Future Use...')
+clf = svm.SVC(gamma=0.1, kernel='poly')
+clf.fit(X_train,y_train)
+
+with open('MNIST_SVM.pickle','wb') as f:
+	pickle.dump(clf, f)
+
+pickle_in = open('MNIST_SVM.pickle','rb')
+clf = pickle.load(pickle_in)
+
+print('\nCalculating Accuracy of trained Classifier...')
+acc = clf.score(X_test,y_test)
+
+print('\nMaking Predictions on Validation Data...')
+y_pred = clf.predict(X_test)
+
+print('\nCalculating Accuracy of Predictions...')
+accuracy = accuracy_score(y_test, y_pred)
+
+print('\nCreating Confusion Matrix...')
+conf_mat = confusion_matrix(y_test,y_pred)
+
+print('\nSVM Trained Classifier Accuracy: ',acc)
+print('\nPredicted Values: ',y_pred)
+print('\nAccuracy of Classifier on Validation Images: ',accuracy)
+print('\nConfusion Matrix: \n',conf_mat)
+
+# Plot Confusion Matrix Data as a Matrix
+plt.matshow(conf_mat)
+plt.title('Confusion Matrix for Validation Data')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+
+print('\nMaking Predictions on Test Input Images...')
+test_labels_pred = clf.predict(test_img)
+
+print('\nCalculating Accuracy of Trained Classifier on Test Data... ')
+acc = accuracy_score(test_labels,test_labels_pred)
+
+print('\n Creating Confusion Matrix for Test Data...')
+conf_mat_test = confusion_matrix(test_labels,test_labels_pred)
+
+print('\nPredicted Labels for Test Images: ',test_labels_pred)
+print('\nAccuracy of Classifier on Test Images: ',acc)
+print('\nConfusion Matrix for Test Data: \n',conf_mat_test)
+
+# Plot Confusion Matrix for Test Data
+plt.matshow(conf_mat_test)
+plt.title('Confusion Matrix for Test Data')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+sys.stdout = old_stdout
+log_file.close()
+
+
+# Show the Test Images with Original and Predicted Labels
+a = np.random.randint(1,40,15)
+for i in a:
+	two_d = (np.reshape(test_img[i], (45, 45)) * 255).astype(np.uint8)
+	plt.title('Original Label: {0}  Predicted Label: {1}'.format(test_labels[i],test_labels_pred[i]))
+	plt.imshow(two_d, interpolation='nearest',cmap='gray')
+	plt.show()
+#---------------------- EOC ---------------------#
+
