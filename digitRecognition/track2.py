5b6df283f96caec95f6d269f4a0b45e5598ed343
= <=>
2017-11-19 04:24:25 +0530

    Final Working code for segementation

--- digitRecognition/track2.py
+++ digitRecognition/track2.py
@@ -34,6 +34,7 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 # smooth the image to avoid noises
 gray = cv2.medianBlur(gray,5)
 
+
 # Apply adaptive threshold
 thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
 thresh_color = cv2.cvtColor(thresh,cv2.COLOR_GRAY2BGR)
@@ -42,15 +43,21 @@ thresh_color = cv2.cvtColor(thresh,cv2.COLOR_GRAY2BGR)
 thresh = cv2.dilate(thresh,None,iterations = 3)
 thresh = cv2.erode(thresh,None,iterations = 2)
 
+# im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
+
+# # Threshold the image
+# ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
+
 # Find the contours
-_,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+_,contours,hierarchy = cv2.findContours(im_th,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 contours = sorted(contours, key=lambda cont: cv2.boundingRect(cont)[0])
 
 X_cord = []
 Y_cord = []
 W_cord = []
 H_cord = []
-labels = ['x','i','y','j']
+labels = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
 # For each contour, find the bounding rectangle and draw it
 for cnt in contours:
     x,y,w,h = cv2.boundingRect(cnt)
@@ -69,14 +76,16 @@ print Y_cord
 print W_cord
 print H_cord
 
+# count  = 0
+
 # process subscripts, superscripts
 start = Node(value=0,label=labels[0])
 parent_avg = (Y_cord[0] + (H_cord[0]/2))
 prev_node = start
 # for i in xrange(1, len(X_cord)):
-for x in xrange(1,10):
-	pass
-def shitfun(prev_node,curr_node):
+
+def shitfun(prev_node,curr_node,count):
+	# count = count +1
 	l_avg = (Y_cord[prev_node.value] + (H_cord[prev_node.value]/2))
 	l_bot = Y_cord[prev_node.value]
 	l_top = l_bot + H_cord[prev_node.value]
@@ -84,109 +93,92 @@ def shitfun(prev_node,curr_node):
 	r_top = r_bot + H_cord[curr_node.value]
 	prnt = prev_node.parent
 	if prnt is None:
+		print "count is ",count,"Inside None"
 		if l_avg < r_bot:
 				# Go to TOP of L 
+			print "count is ",count,"Inside None 1"
+			prev_node.top = curr_node
+			curr_node.parent = prev_node
+			prev_node = curr_node
 		elif l_avg > r_top:
-			# Go to BOT Of L 
+			# Go to BOT Of L
+			print "count is ",count,"Inside None 2" 
+			prev_node.bottom = curr_node
+			curr_node.parent = prev_node
+			prev_node = curr_node
 		else:
 			# GO to Next of L 
-	# new_node = Node(value=i,label=labels[i])
+			print "count is ",count,"Inside None 3"
+			prev_node.nxt = curr_node
+			curr_node.parent = prev_node.parent
+			prev_node = curr_node
+		return prev_node
 	else:
 		parent_avg = (Y_cord[prnt.value] + (H_cord[prnt.value]/2))
 		if parent_avg < l_bot:
+			print "count is ",count,"Inside Parent in down"
 			if r_bot > parent_avg:
 				if r_bot > l_avg and r_bot < l_top:
-					#  Go to TOP of L 
+					#  Go to TOP of L
+					print "TOP"
+					prev_node.top = curr_node
+					curr_node.parent = prev_node
+					prev_node = curr_node
+
 				elif r_top < l_avg: 
 					# Go to Bottom  of L
+					print "Bottom"
+					prev_node.bottom = curr_node
+					curr_node.parent = prev_node
+					prev_node = curr_node
 				elif r_top > l_avg:
 					# Go to next of L
+					print "Next"
+					prev_node.nxt = curr_node
+					curr_node.parent = prev_node.parent
+					prev_node = curr_node
 				else:
-					shitfun(prev_node.parent,curr_node)
+					return shitfun(prev_node.parent,curr_node,count)
+					
 			else:
-				shitfun(prev_node.parent,curr_node)
+				return shitfun(prev_node.parent,curr_node,count)
+			return prev_node
 		elif parent_avg > l_top:
+			print "count is ",count,"Inside Parent is UP"
 			if r_top < parent_avg:
 				if r_top > l_bot and r_top < l_avg:
-					# Go to BOTTOM of L 
+					# Go to BOTTOM of L
+					print "Bottom" 
+					prev_node.bottom = curr_node
+					curr_node.parent = prev_node
+					prev_node = curr_node
 				elif r_bot > l_avg:
-					# Go to TOP of L 
+					# Go to TOP of L
+					print "Top" 
+					prev_node.top = curr_node
+					curr_node.parent = prev_node
+					prev_node = curr_node
 				elif r_bot < l_avg:
-					#  Go to Next of L 
+					#  Go to Next of L
+					print "Next"
+					prev_node.nxt = curr_node
+					curr_node.parent = prev_node.parent
+					prev_node = curr_node 
 				else:
-					shitfun(prev_node.parent,curr_node)
+					return shitfun(prev_node.parent,curr_node,count)
 			else:
-				shitfun(prev_node.parent,curr_node)
-
-
-
-
-	# if l_avg < r_bot:
-	# 	print 'holo 1'
-	# 	
-	# 	if prnt is not None:
-	# 		
-	# 	else:
-	# 		parent_avg = 0	# just shit to make it to go to superscript
-
-	# 	prev_node.top = new_node
-	# 	new_node.parent = prev_node
-	# 	prev_node = new_node
-	# 	# # back to parent
-	# 	# if parent_avg < l_avg:
-	# 	# 	prev_node.top = new_node
-	# 	# 	new_node.parent = prev_node
-	# 	# 	prev_node = new_node
-
-	# 	# if parent_avg < r_bot:
-	# 	# 	prnt.top = new_node
-	# 	# 	new_node.parent = prnt
-	# 	# 	prev_node = new_node
-
-	# 	# elif parent_avg < r_top:
-	# 	# 	prnt.nxt = new_node
-	# 	# 	new_node.parent = prnt.parent
-	# 	# 	prev_node = new_node
-	# 	# else:
-	# 	# 	# superscript in subscript!!!
-			
-	# elif l_avg > r_top:
-	# 	print 'holo 2'
-	# 	prnt = prev_node.parent
-	# 	if prnt is not None:
-	# 		parent_avg = (Y_cord[prnt.value] + H_cord[prnt.value]/2)
-	# 	else:
-	# 		parent_avg = 0	# just shit to make it to go to subscript
-	# 	if parent_avg > r_top:
-	# 		prnt.bottom = new_node
-	# 		new_node.parent = prnt
-	# 		prev_node = new_node
-	# 	prev_node.bottom = new_node
-	# 	new_node.parent = prev_node
-	# 	prev_node = new_node
-	# 	# if parent_avg < l_avg:
-	# 	# 	prev_node.bottom = new_node
-	# 	# 	new_node.parent = prev_node
-	# 	# 	prev_node = new_node
-	# 	# # back to parent
-	# 	# 
-	# 	# elif parent_avg > r_bot:
-	# 	# 	# subscript in superscript!!!
-	# 	# 	prnt.nxt = new_node
-	# 	# 	new_node.parent = prnt.parent
-	# 	# 	prev_node = new_node
-	# else:
-	# 	print 'holo 3'
-	# 	prev_node.nxt = new_node
-	# 	new_node.parent = prev_node.parent
-	# 	prev_node = new_node
-
-printTree(start)
-
-# Finally show the image
-# cv2.imshow('img',img)
+				return shitfun(prev_node.parent,curr_node,count)
+			return prev_node
+cv2.imshow('img',img)
 # cv2.imshow('res',thresh_color)
-# cv2.waitKey()
+cv2.waitKey()
+
+for i in xrange(1,len(X_cord)):
+	curr_node = Node(value=i,label=labels[i])
+	prev_node = shitfun(prev_node,curr_node,i)
+	print "_L-------------------",prev_node.label
+printTree(start)
+# Finally show the image
 # sys.exit(1)
                                            
 # cv2.destroyAllWindows()
